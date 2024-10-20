4c8b539a504aadfbd9040ecd626d5cebef1c6111
tanu sapate <sapatetanu@gmail.com>
2024-10-20 18:17:47 +0530

    commiting  by pratik

---
+++ src/components/home/test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+    Home(){
+            return  <button onClick={this.call}>Click the button!</button>
+
+    }
+  render() {
+    return (
+        test.Home()
+    )
+  }
+}
+ 
